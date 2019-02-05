79756752f42d59a3a20e9fe125bc65146f4cc051
Rens Hamburger <renshamburger@gmail.com>
2019-02-05 18:04:57 +0100

    Import pagination suport

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import BootstrapVue from 'bootstrap-vue';
+import {Pagination} from 'bootstrap-vue/es/components';
 
 Vue.use(BootstrapVue);
+Vue.use(Pagination);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app');
